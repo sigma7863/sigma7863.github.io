f6605cd8a41c323d684ce85e2c86c108d0d91f0e
Ido <mukae.yuta.0811@gmail.com>
2025-04-24 16:43:07 +0900

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 export const metadata: Metadata = {
   title: "シグマの部屋",
-  description: "シグマの個人サイト",
+  description: "シグマのWEBサイトです！",
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   )
 }
