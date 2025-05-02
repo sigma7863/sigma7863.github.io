78378c0f2d60de653a15269b7054768bab585f03
Ido <mukae.yuta.0811@gmail.com>
2025-05-03 07:10:56 +0900

    Analyticsを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type React from "react";
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Clarity from '@microsoft/clarity';
 
 export const metadata: Metadata = {
   title: "シグマの部屋",
@@ -10,12 +10,13 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
+  const projectId = "rdfgm0em5a"
+  Clarity.init(projectId);
+
   return (
     <html lang="ja">
       <body>
         <script defer src="https://cloud.umami.is/script.js" data-website-id="c3579817-4c9c-45e5-aa3f-89f96eb742d8"></script>
-        <script defer data-domain="sigma7863.github.io" src="https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
-        <script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
         <Header/>
           {children}
         <Footer/>
