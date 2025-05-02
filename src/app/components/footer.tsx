8cfe9023d1225fa2fedba8dfd8140392fb47d9cc
Ido <mukae.yuta.0811@gmail.com>
2025-05-02 23:53:13 +0900

    フラグメントを追加

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,13 +1,15 @@
 export default function Footer() {
   return(
-    <footer className="bg-blue-900 text-white text-center py-4">
-      <hr className="border-white my-4" />
-      <p>© 2025 Sigma7863</p>
-      <p>このサイトは<a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">Next.js</a>と
-        <a href="https://tailwindcss.com/" target="" rel="noopener noreferrer">Tailwind CSS</a>と
-        <a href=""></a>
-        を使って作成しました
-      </p>
-    </footer>
+    <>
+      <footer className="bg-blue-900 text-white text-center py-4">
+        <hr className="border-white my-4" />
+        <p>© 2025 Sigma7863</p>
+        <p>このサイトは<a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">Next.js</a>と
+          <a href="https://tailwindcss.com/" target="" rel="noopener noreferrer">Tailwind CSS</a>と
+          <a href=""></a>
+          を使って作成しました
+        </p>
+      </footer>
+    </>
 	);
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,14 +2,16 @@ import Link from 'next/link'
 
 export default function Header (){
   return (
-    <header className="bg-indigo-200 p-4 flex justify-between items-center">
-      <Link href="/"><h1 className="text-purple-800 text-2xl font-medium">シグマの部屋</h1></Link>
-      <nav className="flex gap-6 text-gray-600">
-        <Link href="/" className="hover:text-gray-900">about</Link>
-        <Link href="/blog" className="hover:text-gray-900">blog</Link>
-        <Link href="/works" className="hover:text-gray-900">works</Link>
-        <Link href="/link" className="hover:text-gray-900">Link</Link>
-      </nav>
-    </header>
+    <>
+      <header className="bg-indigo-200 p-4 flex justify-between items-center">
+        <Link href="/"><h1 className="text-purple-800 text-2xl font-medium">シグマの部屋</h1></Link>
+        <nav className="flex gap-6 text-gray-600">
+          <Link href="/" className="hover:text-gray-900">about</Link>
+          <Link href="/blog" className="hover:text-gray-900">blog</Link>
+          <Link href="/works" className="hover:text-gray-900">works</Link>
+          <Link href="/link" className="hover:text-gray-900">Link</Link>
+        </nav>
+      </header>
+    </>  
   );
 }
