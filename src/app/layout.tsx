5a0377565a2cd46450b075c724978d493f9bffdd
Ido <mukae.yuta.0811@gmail.com>
2025-05-03 08:59:40 +0900

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,11 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
-
   return (
     <html lang="ja">
       <body>
         <script defer src="https://cloud.umami.is/script.js" data-website-id="c3579817-4c9c-45e5-aa3f-89f96eb742d8"></script>
+        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ce7d048c0bcb4f008c39484f6c4301bc"}'></script>
         <Header/>
           {children}
         <Footer/>
