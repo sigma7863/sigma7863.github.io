f049eefb2554f8655d37c788daf1b8bfd2248b71
Ido <mukae.yuta.0811@gmail.com>
2025-05-06 07:02:56 +0900

    taeget="_blank"を設定

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <hr className="border-white my-4" />
         <p>© 2025 Sigma7863</p>
         <p>このサイトは<a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">Next.js</a>と
-          <a href="https://tailwindcss.com/" target="" rel="noopener noreferrer">Tailwind CSS</a>と
+          <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">Tailwind CSS</a>と
           <a href=""></a>
           を使って作成しました
         </p>
