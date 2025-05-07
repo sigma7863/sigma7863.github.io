912b65f40b5bbe12e11d81d7ac8c7bdd3cfac2ca
Ido <mukae.yuta.0811@gmail.com>
2025-05-07 21:48:49 +0900

    少し変更

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
       <footer className="bg-blue-900 text-white text-center py-4">
         <hr className="border-white my-3" />
         <p className="text-center inline-flex items-center space-x-1">
-          © 2025 Sigma7863 このサイトの技術スタックは
-          <a href="" className="underline">こちらから</a>
+          © 2025 Sigma7863  このサイトの技術スタックは
+          <a href="" className="underline hover:no-underline">こちらから</a>
           <ExternalLink className="w-4 h-4"/>
         </p>
       </footer>
