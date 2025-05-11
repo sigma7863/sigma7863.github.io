fbd4eebca435bc1d55b200d446b4c7c6f1771c53
Ido <mukae.yuta.0811@gmail.com>
2025-05-11 12:40:08 +0900

    microCMSを導入

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,25 +1,40 @@
-import Link from "next/link";
-import { getArticles } from "@/lib/newt";
-import type { Metadata } from "next";
+// app/page.tsx
+import Link from 'next/link';
+import { client } from '@/libs/microcms';
 
-export const metadata: Metadata = {
-  title: "シグマのブログ",
-  description: "日記や本の感想など、色々書いています",
+// ブログ記事の型定義
+type Props = {
+  id: string;
+  title: string;
+};
+
+// microCMSからブログ記事を取得
+async function getBlogPosts(): Promise<Props[]> {
+  const data = await client.get({
+    endpoint: 'blog', // "blog"はmicroCMSのエンドポイント名
+    queries: {
+      fields: 'id,title',  // idとtitleを取得
+      limit: 5,  // 最新の5件を取得
+    },
+  });
+  return data.contents;
 }
 
-export default async function Blog() {
-  const articles = await getArticles()
+export default async function Home() {
+  const posts = await getBlogPosts();
+
   return (
-     <main className="">
+    <main>
+      <h1>ブログ記事一覧</h1>
       <ul>
-        {articles.map((article) => {
-          return (
-            <li key={article._id}>
-              <Link href={`articles/${article.slug}`}>{article.title}</Link>
-            </li>
-          )
-        })}
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}> {/* 記事へのリンクを生成 */}
+              {post.title} {/* タイトルを表示 */}
+            </Link>
+          </li>
+        ))}
       </ul>
     </main>
-  )
-} 
+  );
+}
