c9fb113892dfe0e35c289d89a3ca2e4bdad75d2a
Ido <mukae.yuta.0811@gmail.com>
2025-05-25 18:42:28 +0900

    いろいろ追加

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { ExternalLink } from 'lucide-react';
 
 export default function Works() {
   return (
@@ -10,24 +11,30 @@ export default function Works() {
         <p className="text-xl mb-8">大したことはしていません</p>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          <Link href="https://example.com"
+          <Link href="https://docs.google.com/spreadsheets/d/1CHVfHxc5I3HMjK6TT19GLWB0EyuDyx4iaTRm2h4iTQs/edit?usp=sharing"
             target="_blank"
-            className="bg-white text-black rounded-2xl shadow-md overflow-hidden hover:scale-105 transition-transform duration-300"
+            className="bg-[#c2cafc] text-black rounded-2xl overflow-hidden hover:scale-101 transition-transform duration-180"
           >
             <Image src="/splatoon3-spreadsheet.png" width={40} height={40} alt="splatoon3-spreadsheet" className="w-full h-auto"/>
             <div className="p-4">
               <h2 className="text-lg font-bold">スプラトゥーン3 スプレッドシート</h2>
               <p className="text-sm mt-1">
-                スプラ3のガチャやクキチドローンで出た結果などの統計を取っているスプレッドシートです
+                スプラ3のガチャやブキチドローンで出た結果などの統計を取っているスプレッドシートです
               </p>
-              <p className="text-sm text-blue-600 mt-2">
-                クリックしてアクセス ↗
-              </p>
+              <a href="https://docs.google.com/spreadsheets/d/1CHVfHxc5I3HMjK6TT19GLWB0EyuDyx4iaTRm2h4iTQs/edit?usp=sharing"
+                target="_blank"
+                className="inline-flex items-center text-base text-blue-600 text-right mt-2 hover:underline"
+              >
+                クリックしてアクセス
+                <ExternalLink className="w-4 h-4" />
+              </a>
             </div>
           </Link>
-
-          {/* 空のプレースホルダーカード */}
-          <div className="bg-[#c2cafc] rounded-2xl aspect-square" />
+          
+          <div className="bg-[#cdd2fb] rounded-2xl aspext-square flex flex-col items-center justify-center text-center shadow-md">
+            <p className="text-black text-xl mb-2">？？？</p>
+            <h2 className="text-black text-lg font-bold">ComingSoon...</h2>
+          </div>
           <div className="bg-[#c2cafc] rounded-2xl aspect-square" />
         </div>
       </main>
