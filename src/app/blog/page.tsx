04679baa476b4529dc5bf7dff92f2fe07ab00227
Ido <mukae.yuta.0811@gmail.com>
2025-05-25 18:42:12 +0900

    ブログを一旦削除

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,25 +1,15 @@
-import Link from "next/link"
-import { getArticles } from "@/lib/newt"
-import type { Metadata } from "next"
+// import Link from "next/link"
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Blog",
   description: "シグマのブログです",
 }
 
-export default async function Home() {
-  const articles = await getArticles()
+export default async function Blog() {
   return (
     <main className="">
-      <ul>
-        {articles.map((article) => {
-          return (
-            <li key={article._id}>
-              <Link href={`articles/${article.slug}`}>{article.title}</Link>
-            </li>
-          )
-        })}
-      </ul>
+      
     </main>
   )
 }
