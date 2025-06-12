7897eb6bd85ab543c7a33e7b275f26b6afbf1975
Ido <mukae.yuta.0811@gmail.com>
2025-06-13 02:02:31 +0900

    faviconとアナリティクスを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Head from "next/head";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -11,9 +13,12 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="ja">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
-        <script defer src="https://cloud.umami.is/script.js" data-website-id="c3579817-4c9c-45e5-aa3f-89f96eb742d8"></script>
-        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ce7d048c0bcb4f008c39484f6c4301bc"}'></script>
+        <Script src="https://cloud.umami.is/script.js" data-website-id="c3579817-4c9c-45e5-aa3f-89f96eb742d8"></Script>
+        <Script src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ce7d048c0bcb4f008c39484f6c4301bc"}'></Script>
         <Header/>
           {children}
         <Footer/>
