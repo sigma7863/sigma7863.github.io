368749e43dc79ca0f8600cd4ece502869277e4c5
Ido <mukae.yuta.0811@gmail.com>
2025-06-16 07:05:59 +0900

    微調整

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -10,7 +10,7 @@ export default function Works() {
           <p className="text-xl mb-1">過去に作ったやつを載せています</p>
           <p className="text-xl mb-8">大したことはしていません</p>
           
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 items-stretch">
             <Link href="https://docs.google.com/spreadsheets/d/1CHVfHxc5I3HMjK6TT19GLWB0EyuDyx4iaTRm2h4iTQs/edit?usp=sharing"
               target="_blank"
               className="bg-[#c2cafc] text-black rounded-2xl overflow-hidden hover:scale-102 transition-transform duration-180"
@@ -24,13 +24,13 @@ export default function Works() {
               </div>
             </Link>
             
-            <div className="bg-[#cdd2fb] rounded-2xl aspect-square flex flex-col items-center justify-center text-center shadow-md">
+            <div className="flex flex-col bg-[#cdd2fb] rounded-2xl p-4 h-full min-h-[265px] justify-center items-center text-center shadow-md">
               <p className="text-black text-xl mb-2">？？？</p>
               <h2 className="text-black text-lg font-bold">ComingSoon...</h2>
               <h2 className="text-black text-lg">一応やりたいことは決まっている</h2>
             </div>
             
-            <div className="bg-[#c2cafc] rounded-2xl aspext-square" />
+            <div className="bg-[#c2cafc] rounded-2xl h-full" />
           </div>
         </div>
       </main>
