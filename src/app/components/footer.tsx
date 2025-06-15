e25c022dfbbf383e64269f4e4414aa8a018a6259
Ido <mukae.yuta.0811@gmail.com>
2025-06-16 03:55:30 +0900

    レスポンシブ対応

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,18 +1,21 @@
 import { ExternalLink } from 'lucide-react';
 
 export default function Footer() {
-  return(
+  return (
     <>
       <footer className="bg-blue-900 text-white text-center py-4">
         <hr className="border-white my-3" />
-        <p className="text-center inline-flex items-center space-x-1">
-          © 2025 Sigma7863 このサイトの技術スタックは
-          <a href="/techstack" className="inline-flex items-center space-x-1 ml-1 underline hover:no-underline">
-            <span>こちらから</span>
-            <ExternalLink className="w-4 h-4"/>
-          </a>
+        <p className="text-center">
+          © 2025 Sigma7863{" "}
+          <span className="whitespace-nowrap">
+            このサイトの技術スタックは
+            <a href="/techstack" className="inline-flex items-center space-x-1 ml-1 underline hover:no-underline">
+              <span>こちらから</span>
+              <ExternalLink className="w-4 h-4" />
+            </a>
+          </span>
         </p>
       </footer>
     </>
-	);
+  );
 }
