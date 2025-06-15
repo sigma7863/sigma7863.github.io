91ee904f4a42d92b409f7fdee838c5be1e0e60bd
Ido <mukae.yuta.0811@gmail.com>
2025-06-16 03:56:27 +0900

    blogの画面を追加

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,3 @@
-// import Link from "next/link"
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,10 +5,17 @@ export const metadata: Metadata = {
   description: "シグマのブログです",
 }
 
-export default async function Blog() {
+export default function Blog() {
   return (
-    <main className="">
-      
-    </main>
-  )
+    <>  
+      <main className="min-h-screen bg-blue-900 text-white">
+        <div className="max-w-4xl mx-auto px-4 py-8">
+          <section>
+            <h2 className="text-3xl font-bold mb-2">Blog</h2>
+            <p className="text-xl mb-6">書くか書かないかは未定...</p>
+          </section>
+        </div>  
+      </main>
+    </>  
+  );
 }
