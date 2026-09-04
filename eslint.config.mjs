294867de1f25f84cd674f7044dda20c846dfc0d4
Sigma) <mukae.yuta.0811@gmail.com>
2026-09-04 14:24:46 +0900

    Update site dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginImport from "eslint-plugin-import";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
-   {
-    plugins: {
-      import: eslintPluginImport,
-    },
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
     settings: {
       "import/resolver": {
         typescript: {
-          project: "./tsconfig.json", // tsconfigのパスを指定
+          project: "./tsconfig.json",
         },
       },
     },
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
